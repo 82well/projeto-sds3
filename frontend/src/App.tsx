4262c7ead2b6228d83e742925bb93429bb8e1a25
Wellington <nylook2@hotmail.com>
2021-11-02 18:09:09 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/NavBar/BarChart";
 import DataTable from "components/NavBar/DataTable";
+import DonutChart from "components/NavBar/DonutChart";
 import Footer from "components/NavBar/Footer";
 import NavBar from "components/NavBar/idex";
 
@@ -8,8 +10,25 @@ function App() {
       <NavBar />
 
       <div className="container">
-        <h1 className="text-primary">Olá Mundo</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas Vendas</h5>
+            <DonutChart />
+          </div>
+
+
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
 
         <DataTable />
       </div>
