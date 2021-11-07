60e5c394ddfd95377b95ffc6805c4a308cb1f9a6
Wellington <nylook2@hotmail.com>
2021-11-07 09:23:33 -0300

    First request

--- frontend/src/components/NavBar/idex.tsx
+++ frontend/src/components/NavBar/idex.tsx
@@ -7,9 +7,9 @@ function NavBar() {
                 <nav className="my-2 my-md-0 mr-md-3">
 
                     <Link to="/">
-                    <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                     <img src={ImgDsDark} alt="DevSuperior" width="120" />
                     </Link>
-                    
+
                 </nav>
             </div>
         </div>
